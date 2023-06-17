7e3c6d7b3ae32db205ebc662f4260523ef18933c
jolanowak <122167686+jolanowak@users.noreply.github.com>
2023-06-17 08:54:25 +0200

    Add files via upload

---
+++ Api/WebApplication1/Models/PeopleContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Models
+{ //baza w pamięci po restarcie sie resetuje
+    public class PeopleContext : DbContext
+    {
+        public DbSet<People> People { get; set; }
+
+        public PeopleContext(DbContextOptions<PeopleContext> options) : base(options)
+        {
+
+        }
+    }
+}
+//instalacja - Install-Package Microsoft.EntityFrameworkCore.InMemory
